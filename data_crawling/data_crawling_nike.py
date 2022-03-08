448139d350df2f0c2089af6ed6b3ce48ce4467aa
Mehmet Üstek <mustek17@ku.edu.tr>
2022-03-09 02:22:57 +0300

    Nike Data Crawled

--- data_crawling/data_crawling_nike.py
+++ data_crawling/data_crawling_nike.py
@@ -4,15 +4,12 @@ from time import sleep
 
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
+from selenium.common.exceptions import NoSuchElementException
 import numpy as np
 import csv
 
-name_list = []
-image_list = []
-price_list = []
-description_list = []
 
-sleep_time = 2
+sleep_time = 1
 
 
 def write_to_csv(filename, name, imageId, price, description):
@@ -24,52 +21,56 @@ def write_to_csv(filename, name, imageId, price, description):
 
 # Crawling part
 driver = webdriver.Chrome()
-driver.set_window_size(700, 1080)
+driver.set_window_size(700, 970)
+
 
 # # Start the process, wait for website to load.
 driver.get("https://www.nike.com/w/mens-shoes-nik1zy7ok")
+
 # Initial wait
 sleep(sleep_time+ 10)
+body = driver.find_element(by=By.CSS_SELECTOR, value='body')
+for _ in range(150):
+    body.send_keys(Keys.PAGE_DOWN)
+    sleep(0.4)
 
-for index in range(11,588):
-    if index == 50 or index == 80 or index == 81:
-        continue
+for index in range(288,588):
     # For scrolling down.
+    sleep(sleep_time*1.5)
+
     body = driver.find_element(by=By.CSS_SELECTOR, value='body')
     for _ in range(2):
         body.send_keys(Keys.PAGE_DOWN)
-        sleep(1)
-    sleep(sleep_time-2)
+        sleep(0.7)
+    sleep(sleep_time)
     driver.find_elements(by=By.XPATH, value='//*[@id="Wall"]/div/div/div/main/section/div/div/div/figure/a[2]')[index].click()
+    try:
+        temp = driver.find_element(by=By.XPATH, value=
+        '//*[@id="PDP"]/div/div[3]/div[1]/div/div[2]/div/div/div/div/div')
+        price = temp.text
+        description = driver.find_element(by=By.XPATH, value='//*[@id="PDP"]/div/div[3]/div[1]/div/div[2]/div/h2').text
+        name = driver.find_element(by=By.XPATH, value=
+            '//*[@id="pdp_product_title"]').text
 
-    price = driver.find_element(by=By.XPATH, value=
-        '//*[@id="PDP"]/div/div[3]/div[1]/div/div[2]/div/div/div/div/div').text
-    price_list.append(price)
-    description = driver.find_element(by=By.XPATH, value='//*[@id="PDP"]/div/div[3]/div[1]/div/div[2]/div/h2').text
-    description_list.append(description)
-    name = driver.find_element(by=By.XPATH, value=
-        '//*[@id="pdp_product_title"]').text
-    name_list.append(name)
-
-    sleep(sleep_time)
-    for image_index in range(1, 5):
-        value = '//*[@id="PDP"]/div/div[3]/div[2]/div[1]/div/div/ul/li[' + str(
-            image_index) + ']/span/button/div/picture[2]/img'
-        image = driver.find_element(by=By.XPATH,
-                                    value=value)
-        src = image.get_attribute('src')
-        response = requests.get(src)
-        image_name = "images/image" + str(index) + "sub" + str(image_index) + ".png"
-        file = open(image_name, "wb")
-        file.write(response.content)
-        file.close()
-    imageId = "imageId" + str(index)
-    image_list.append(imageId)
-    driver.back()
-    sleep(sleep_time)
-    stack = np.stack((name, imageId, price, description))
-    print(stack)
-    write_to_csv("data.csv", name, imageId, price, description)
+        for image_index in range(1, 5):
+            value = '//*[@id="PDP"]/div/div[3]/div[2]/div[1]/div/div/ul/li[' + str(
+                image_index) + ']/span/button/div/picture[2]/img'
+            image = driver.find_element(by=By.XPATH,
+                                        value=value)
+            src = image.get_attribute('src')
+            response = requests.get(src)
+            image_name = "images/image" + str(index) + "sub" + str(image_index) + ".png"
+            file = open(image_name, "wb")
+            file.write(response.content)
+            file.close()
+        imageId = "imageId" + str(index)
+        driver.back()
+        stack = np.stack((name, imageId, price, description))
+        print(stack)
+        write_to_csv("data.csv", name, imageId, price, description)
+    except NoSuchElementException:
+        driver.back()
+        continue
 
 driver.close()
 
