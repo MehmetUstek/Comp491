6b93b7c5b5dbb1f7fccd91e254683bb66aa65bdf
serhat-coskun <scoskun17@ku.edu.tr>
2022-03-29 00:23:55 +0300

    Finalized transfer learning extractors

--- artemis/src/compare/image_extraction.py
+++ artemis/src/compare/image_extraction.py
@@ -6,7 +6,7 @@
 # Provides utility to compare feature vectors
 
 
-#imports
+# imports
 
 # tensorflow realted libraries
 from tensorflow import keras
@@ -19,29 +19,44 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 # defines cosine distance
-from scipy import spatial 
+from scipy import spatial
 
 # pretrained models
 from tensorflow.keras.applications import (
-        vgg16,
-        resnet50,
-        mobilenet,
-        inception_v3
-    )
+    vgg16,
+    resnet50,
+    mobilenet,
+    inception_v3
+)
+"""
+  Globals
+"""
+
+methods = dict()
+
+# Init vgg16, load imagenet pretrained weights
+vgg16_model = vgg16.VGG16(weights='imagenet', include_top=False)
+# Init resnet50
+resnet50_model = resnet50.ResNet50(weights='imagenet', include_top=False)
 
 
+def as_extractor(method_name):
+    def decorator(func):
+        methods[method_name] = func
+        return func
+
+    return decorator
 
 
+# TODO Implement normalization routin
+def compare(feature1, feature2, type="cosine"):
+    """ Returns a similarity measure, note that it is not normalized
 
-
-def compare(feature1, feature2):
-    """ Returns cosine similarity between feature vectors
-    
     Parameters
     ----------
     feature1: list<number>, feature vector
     feature2: list<number>, feature vector
-    
+    type:     string, l1, l2, or cosine similarity measure
     Raises
     ------
     ValueError
@@ -49,61 +64,91 @@ def compare(feature1, feature2):
     """
     if len(feature1) != len(feature2):
         raise ValueError("Features are not of same length!")
-    
-    return 1 - spatial.distance.cosine(feature1, feature2) 
+
+    if type == "l1":
+        return np.linalg.norm(feature1 - feature2, ord=1)
+
+    if type == "l2":
+        return np.linalg.norm(feature1 - feature2)
+
+    return 1 - spatial.distance.cosine(feature1, feature2)
 
 
+@as_extractor("vgg16")
+def extract_vgg16(img):
+    """ Returns feature vector, extracted using vgg16
 
-def extract(image, methods=list("all")):
-    """ Returns an dictionary of vectors, where keys are feature type and values are feature vectors
-    
-    If argument methods is not passed, all is assumed. 'all' implies every avaliable method will be 
-    used to return feature vectors.
-    
     Parameters
     ----------
-    
-    
+    img: PIL format image
+
     """
-
-def init_model(self, model_name):
-    
-    if model_name == "resnet":
-        pass
-    
-    if model_name == "vggnet":
-        from tensorflow.keras.applications import (vgg16)
-        layer_name = "fc1"
-        model = vgg16.VGG16(weights='imagenet')
-        intermediate_layer_model = keras.Model(inputs=model.input, outputs=model.get_layer(layer_name).output)
-
-            
-    
-# Returns the features for image in the filename
-def get_features(self, filename):
-    
-    ### Note that target size is model specific
-    # I am dirty coding for fast prototyping
-    img = load_img(filename, target_size = (224, 224))
+    # Prepare image as an input
     numpy_img = img_to_array(img)
     image_batch = np.expand_dims(numpy_img, axis=0)
-    
+
     # preprocess the img, based on pre-trained model's procedure
     # Need to research how to make this part programatic
     processed_image = vgg16.preprocess_input(image_batch.copy())
-    predictions = model.predict(processed_image)
-    return predictions
-    
+    feature_vector = vgg16_model.predict(processed_image)
+    return feature_vector
 
+
+@as_extractor("resnet50")
+def extract_resnet(img):
+    """ Returns feature vector, extracted using resnet5*
+
+    Parameters
+    ----------
+    img: PIL format image
+
+    """
+    # Prepare image as an input
+    numpy_img = img_to_array(img)
+    image_batch = np.expand_dims(numpy_img, axis=0)
+
+    # preprocess the img, based on pre-trained model's procedure
+    # Need to research how to make this part programatic
+    processed_image = resnet50.preprocess_input(image_batch.copy())
+    feature_vector = resnet50_model.predict(processed_image)
+    return feature_vector
+
+
+def extract(img, extractors=("all")):
+    """Returns an dictionary of vectors, where keys are feature type and values are feature vectors
+
+    If argument methods is not passed, all is assumed. 'all' implies every avaliable method will be
+    used to return feature vectors.    
+
+    Parameters
+    ----------
+    img: PIL format image
+    extractors: tuple of methods to cretae feature vectors
+
+    """
+    features = dict()
+
+    if "all" in methods:
+        for name, extractor in methods:
+            features[name] = extractor(img)
+        return features
+
+    for name in extractors:
+        features[name] = methods[name](img)
+    return features
+
+
+""" If needed for future reference
 
 layer_name = "fc1"
 model = vgg16.VGG16(weights='imagenet')
-intermediate_layer_model = keras.Model(inputs=model.input, outputs=model.get_layer(layer_name).output) 
+intermediate_layer_model = keras.Model(
+    inputs=model.input, outputs=model.get_layer(layer_name).output)
 model = intermediate_layer_model
 model.summary()
 # load img
-img1 = load_img('./img1.jpg', target_size = (224, 224))
-img2 = load_img('./img2.jpg', target_size = (224, 224))
+img1 = load_img('./img1.jpg', target_size=(224, 224))
+img2 = load_img('./img2.jpg', target_size=(224, 224))
 
 numpy_img = img_to_array(img1)
 image_batch = np.expand_dims(numpy_img, axis=0)
@@ -115,3 +160,4 @@ processed_image2 = vgg16.preprocess_input(image_batch.copy())
 
 p2 = model.predict(processed_image2)
 p1 = model.predict(processed_image1)
+"""
