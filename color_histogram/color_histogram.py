70ea6a39b64319a06329cdf4856a0f848f400ff5
Encatadoo <bakbulut96@gmail.com>
2022-05-19 15:41:23 +0300

    updated color and resnet

--- color_histogram/color_histogram.py
+++ color_histogram/color_histogram.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 import PIL
 from sklearn.cluster import KMeans
 
-N=10
+N=16
 
 def get_cluster_results(k_cluster):
     n_pixels = len(k_cluster.labels_)
@@ -23,7 +23,7 @@ def get_image_color_features(masked_img):
     image_arr = np.reshape(image_arr, (-1, 4))
     image_arr = image_arr[image_arr[:,3] > 50][:,:3]
     
-    clt = KMeans(n_clusters=N)
+    clt = KMeans(n_clusters=N, n_init=20)
     clt_1 = clt.fit(image_arr)
     colors, percentages = get_cluster_results(clt_1)
     return colors, percentages
--- rest_api/data_usage.py
+++ rest_api/data_usage.py
@@ -19,35 +19,34 @@ def parse_json(data):
 
 def getProductsIdWithResnetandColorVector():
     db = cn.connectToDB()
-    #arr1 = []
+    arr1 = []
     arr2 = []
     products_collection: Collection = db.get_collection('Products')
     products = products_collection.find()
     for prod in products:
         product = parse_json(prod)
-        #arr1.append((product['Pid'], pickle.loads(base64.decodebytes((product['resnet50']["$binary"])['base64'].encode()))))
+        arr1.append((product['Pid'], pickle.loads(base64.decodebytes((product['renet50-colorless']["$binary"])['base64'].encode()))))
         arr2.append((product['Pid'], pickle.loads(base64.decodebytes((product['color']["$binary"])['base64'].encode())),
                     pickle.loads(base64.decodebytes((product['percentage']["$binary"])['base64'].encode()))))
 
-    return arr2
+    return arr1, arr2
 
 def getResNetandColorScores(id):
     scores1 = []
     scores2 = []
     ind = 0
-    #all_res, all_clr = getProductsIdWithResnetandColorVector()
-    all_clr = getProductsIdWithResnetandColorVector()
+    all_res, all_clr = getProductsIdWithResnetandColorVector()
     for j in range(len(all_clr)):
         if id == all_clr[j][0]:
             ind = j
             break
 
     for i in range(len(all_clr)):
-        #score1 = np.abs(ex.compare(all_res[ind][1], all_res[i][1]))
+        score1 = np.abs(ex.compare(all_res[ind][1], all_res[i][1]))
         score2 = clr.find_similiarity_score(all_clr[ind][1], all_clr[ind][2], all_clr[i][1], all_clr[i][2])
-        #scores1.append((all_res[i][0], score1))
+        scores1.append((all_res[i][0], -score1))
         scores2.append((all_clr[i][0], -score2))
-    return scores2
+    return scores1, scores2
 
 # def getColorScores():
 #     scores = []
@@ -58,21 +57,21 @@ def getResNetandColorScores(id):
 #         scores.append((all_clr[i+1][0], -score))
 #     return scores
 
-def normalize_arrays_and_weighted_avg(vector2):
+def normalize_arrays_and_weighted_avg(vector1, vector2):
     ans = []
-    #vec1_arr = []
+    vec1_arr = []
     vec2_arr = []
     for i in range(len(vector2)):
-        #vec1_arr.append(vector1[i][1])
+        vec1_arr.append(vector1[i][1])
         vec2_arr.append(vector2[i][1])
 
-    #m1 = np.mean(np.array(vec1_arr))
-    #std1 = np.std(np.array(vec1_arr))
+    m1 = np.mean(np.array(vec1_arr))
+    std1 = np.std(np.array(vec1_arr))
     m2 = np.mean(np.array(vec2_arr))
     std2 = np.std(np.array(vec2_arr))
 
-    for j in range(len(vector2)):
-        ans.append((vector2[j][0], ((vector2[j][1] - m2)/std2)))
+    for j in range(len(vector1)):
+        ans.append((vector1[j][0], ((vector1[j][1] - m1)/std1)*0.0 + ((vector2[j][1] - m2)/std2)*1.0))
 
     return ans
 
@@ -83,15 +82,15 @@ def get_k_min(scores, k):
         order = ['distance'], axis = 0)
 
 def best_ones_ids(id):
-    color_scores = getResNetandColorScores(id)
-    arr = normalize_arrays_and_weighted_avg(color_scores)
+    res_scores, color_scores = getResNetandColorScores(id)
+    arr = normalize_arrays_and_weighted_avg(res_scores, color_scores)
     new = get_k_min(arr, 10)
     ret = []
     for item in new:
         ret.append(item[0])
     return ret
 
-print(best_ones_ids("526"))
+print(best_ones_ids("60"))
 
 
 
